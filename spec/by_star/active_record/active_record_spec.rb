41af1db27dfa1e412cecf23a3428cee7e0b7f3d2
Faraz Yashar <faraz.yashar@gmail.com>
2013-07-25 05:15:24 -0400

    Organize line orders of ActiveRecord spec configs

--- spec/by_star/active_record/active_record_spec.rb
+++ spec/by_star/active_record/active_record_spec.rb
@@ -1,24 +1,22 @@
 require 'spec_helper'
+
+db_config = YAML::load_file(File.dirname(__FILE__) + "/../../database.yml")
+if db_config.has_key?('sqlite') && db_config['sqlite'].has_key?('database')
+  db_config['sqlite']['database'] = File.dirname(__FILE__) + '/../../tmp/' + db_config['sqlite']['database']
+end
+
+ActiveRecord::Base.default_timezone = :utc
+ActiveRecord::Base.configurations = db_config
+ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/../../tmp/activerecord.log")
+ActiveRecord::Base.establish_connection(ENV["DB"] || "sqlite")
+
+load File.dirname(__FILE__) + "/../../fixtures/active_record/schema.rb"
+load File.dirname(__FILE__) + "/../../fixtures/active_record/models.rb"
+load File.dirname(__FILE__) + "/../../fixtures/active_record/seeds.rb"
+
 Dir[File.dirname(__FILE__) + '/../shared/*.rb'].each {|file| require file }
 
 describe ActiveRecord do
-  before(:all) do
-    ActiveRecord::Base.default_timezone = :utc
-
-    db_config = YAML::load_file(File.dirname(__FILE__) + "/../../database.yml")
-    if db_config.has_key?('sqlite') && db_config['sqlite'].has_key?('database')
-      db_config['sqlite']['database'] = File.dirname(__FILE__) + '/../../tmp/' + db_config['sqlite']['database']
-    end
-
-    ActiveRecord::Base.configurations = db_config
-    ActiveRecord::Base.establish_connection(ENV["DB"] || "sqlite")
-    load File.dirname(__FILE__) + "/../../fixtures/active_record/schema.rb"
-    load File.dirname(__FILE__) + "/../../fixtures/active_record/models.rb"
-    load File.dirname(__FILE__) + "/../../fixtures/shared/seeds.rb"
-
-    ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/../../tmp/activerecord.log")
-  end
-
   it_behaves_like "by day"
   it_behaves_like "by direction"
   it_behaves_like "by fortnight"
@@ -35,16 +33,18 @@ describe ActiveRecord do
 
   describe "#between" do
     it "should return an ActiveRecord::Relation object" do
-      Post.between(Date.today - 2, Date.today).class.should == ActiveRecord::Relation
+      Post.between(2.days.ago, Date.today).class.should == ActiveRecord::Relation
     end
     it "should return a result set between two times" do
-      Post.between(Date.today - 2, Date.today).count.should == 1
+      create_posts_relative_to_today
+      Post.between(2.days.ago, Date.today).count.should == 2
     end
   end
 
   describe "#between_times" do
     it "should be an alias of #between" do
-      Post.between_times(Date.today - 2, Date.today).should == Post.between(Date.today - 2, Date.today)
+      create_posts_relative_to_today
+      Post.between_times(2.days.ago, Date.today).should == Post.between(2.days.ago, Date.today)
     end
   end
 end
