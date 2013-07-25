1015024057492deffea613b099f051d5f7f6e0bd
Faraz Yashar <faraz.yashar@gmail.com>
2013-07-25 05:44:41 -0400

    By day specs passing radar/by_star#30

--- spec/by_star/shared/by_day.rb
+++ spec/by_star/shared/by_day.rb
@@ -1,62 +1,77 @@
 require 'spec_helper'
 
 shared_examples_for "by day" do
-  describe "by day" do
-    def posts_count(*args)
-      Post.by_day(*args).count
-    end
 
+  describe "#by_day" do
     it "should be able to find a post for today" do
-      posts_count.should eql(4)
+      posts = Array.new(2) { Post.factory "Today's post", now }
+      create_posts_around now
+      Post.by_day.should == posts
     end
 
     it "should be able to find a post by a given date in last year" do
-      posts_count(:year => Time.zone.now.year - 1).should eql(1)
+      posts = Array.new(2) { Post.factory "This time, last year", 1.year.ago }
+      create_posts_around 1.year.ago
+      Post.by_day(:year => (Time.zone.now.year - 1)).all.should == posts
     end
 
     it "should be able to use an alternative field" do
-      Event.by_day(Time.now.yesterday, :field => "start_time").size.should eql(1)
+      events = Array.new { Event.factory("Yesterday", 1.day.ago) }
+      create_events_around 1.day.ago
+      Event.by_day(1.day.ago, :field => "start_time").all.should  == events
     end
 
     it "should be able to use a date" do
-      posts_count(Date.today).should eql(4)
+      posts = Array.new(2) { Post.factory "Today's post", now }
+      Post.by_day(Date.today).all.should == posts
     end
 
-    it "should be able to use a String" do
-      posts_count(Date.today.to_s).should eql(4)
+    it "should be able to use a standard date String" do
+      posts = Array.new(2) { Post.factory "Today's post", now }
+      create_posts_around now
+      Post.by_day(Date.today.to_s).all.should == posts
     end
   end
 
-  describe "today" do
+  describe "#today" do
     it "should show the post for today" do
-      Post.today.map(&:text).should include("Today's post")
+      posts = Array.new(2) { Post.factory("Today's post", now) }
+      create_posts_around now
+      Post.today.all.should == posts
     end
 
     it "should be able to use an alternative field" do
-      # Test may occur on an event day.
-      Event.today(:field => "start_time").size.should eql(1)
+      events = Array.new(2) { Event.factory("Today", now) }
+      create_events_around now
+      Event.today(:field => "start_time").all.should == events
     end
   end
 
-  describe "yesterday" do
-
+  describe "#yesterday" do
     it "should show the post for yesterday" do
-      Post.yesterday.map(&:text).should include("Yesterday's post")
+      posts = Array.new(2) { Post.factory("Yesterday's post", 1.day.ago) }
+      create_posts_around 1.day.ago
+      Post.yesterday.all.should == posts
     end
 
     it "should be able to use an alternative field" do
-      Event.yesterday(:field => "start_time").size.should eql(1)
+      events = Array.new(2) { Event.factory("Yesterday", 1.day.ago) }
+      create_events_around 1.day.ago
+      Event.yesterday(:field => "start_time").all.should == events
     end
-
   end
 
-  describe "tomorrow" do
+  describe "#tomorrow" do
     it "should show the post for tomorrow" do
-      Post.tomorrow.map(&:text).should include("Tomorrow's post")
+      posts = Array.new(2) { Post.factory("Tomorrow's post", 1.day.from_now) }
+      create_posts_around 1.day.from_now
+      Post.tomorrow.all.should == posts
     end
 
     it "should be able to use an alternative field" do
-      Event.tomorrow(:field => "start_time").size.should eql(1)
+      events = Array.new(2) { Event.factory("T", 1.day.from_now) }
+      create_events_around 1.day.from_now
+      Event.tomorrow(:field => "start_time").all.should == events
     end
   end
 end
